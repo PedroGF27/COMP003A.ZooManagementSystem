8518f0c80b327b346823111222c41dae55e5520d
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-02-23 17:43:48 -0800

    Add project files.

---
+++ COMP003A.ZooManagementSystem/Program.cs
@@ -0,0 +1,14 @@
+﻿// Author: Pedro Garcia
+// Course: COMP-003A
+// Faculty: Johnathan Cruz
+// Purpose: Zoo management system 
+namespace COMP003A.ZooManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
