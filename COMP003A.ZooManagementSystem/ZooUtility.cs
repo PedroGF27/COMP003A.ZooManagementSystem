7183e8c40cc1c7e33e16b2c044a46dc4b44474e3
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-02-23 18:55:27 -0800

    added ZooUtility class to dosplay age, name, and species

---
+++ COMP003A.ZooManagementSystem/ZooUtility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.ZooManagementSystem
+{
+    internal class ZooUtility
+    {
+        public void DescribeAnimal(string Name)
+        {
+            Console.WriteLine($"Animal Name: {Name}");
+        }
+
+        public void DescribeAnimal(string Name, string Species)
+        {
+            Console.WriteLine($"Animal Name: {Name}, Species: {Species}");
+        }
+
+        public void DescribeAnimal(string Name, string Species, int Age)
+        {
+            Console.WriteLine($"Animal Name: {Name}, Species: {Species}, Age: {Age} years old");
+        }
+    }
+}
