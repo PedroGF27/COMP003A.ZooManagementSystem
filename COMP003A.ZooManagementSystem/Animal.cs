8627116a69ff1a57731cfcedbf6de59f1af59400
Pedro Garcia <pedrogarcia5150@my.whccd.edu>
2025-02-23 18:09:45 -0800

    Created abstract Animal class

---
+++ COMP003A.ZooManagementSystem/Animal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.ZooManagementSystem
+{
+    abstract class Animal
+    {
+        private string _name;
+        private string _species;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; 
+            if (string.IsNullOrEmpty(value))
+            {
+                    throw new ArgumentNullException("Error, Please Input a Name.");
+            }
+            }
+        }
+
+        public string Species
+        {  
+            get { return _species; } 
+            set { _species = value; 
+            if (string.IsNullOrEmpty(value))
+            {
+                    throw new ArgumentNullException("Error, Please Enter a Species");
+            }
+            } 
+        }
+
+        /// <summary>
+        /// To contruct animals
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <param name="Species"></param>
+        public Animal(string Name, string Species)
+        {
+            _name = Name;
+            _species = Species;
+        }
+
+        public abstract void MakeSound();
+        
+    }
+}
